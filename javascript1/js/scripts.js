3316eeaffa092ae85a6ad08d0a1218c4901a74a8
brad fraizer <bradfraizer559@gmail.com>
2018-07-02 20:37:15 -0700

    commit before working on takehome

---
+++ javascript1/js/scripts.js
@@ -0,0 +1,25 @@
+//DECLARE A VARIBALE
+var firstName;
+
+//DEFINE YOUR VARIABLE
+firstName = "Brad"; //String
+
+//DEFINE AND DECLARE YOUR VARIABLE
+var lastName = "Fraizer";
+
+console.log(firstName, lastName);
+
+//SINGLE LINE COMMENT
+//firstName = "Beeflaser";
+
+//MULTILINE COMMENT
+/*console.log(firstName);*/
+
+
+//SCOPED INDENTING
+/*
+{
+  //NEW SCOPE
+  var myName;
+}
+*/

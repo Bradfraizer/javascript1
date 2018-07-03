d42bb9b1bc2054c691a1a25686b8d673f41e223e
brad fraizer <bradfraizer559@gmail.com>
2018-07-02 21:01:56 -0700

    first commit day2

---
+++ javascript1/js/scripts.js
@@ -0,0 +1,11 @@
+var myString = "Hello, World!*76";
+console.log(typeof myString);
+var myNumber = "43";
+console.log(typeof myNumber);
+
+var newVariable;
+console.log(newVariable);
+
+var isLoggedIn = false; //BOOLEAN
+
+isLoggedIn = null; //NULL

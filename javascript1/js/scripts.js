bb63f9c593a3e5790808567a41b78c5d4ad2b39b
brad fraizer <bradfraizer559@gmail.com>
2018-07-03 21:12:03 -0700

    verbiage change in log message

--- javascript1/js/scripts.js
+++ javascript1/js/scripts.js
@@ -6,4 +6,4 @@
   greetMe("Ryeker");
 })()
 
-console.log("I'd like to gain proficiency with JavaScript so that I can begin a career in programming development.  My hobbies include board games, video games, and music.");
+console.log("I'd like to gain proficiency with JavaScript so that I can begin a career in web and program development.  My hobbies include board games, video games, and music.");

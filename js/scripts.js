d9b57aafe2d6ccf9085b37a7ca41ae64c744059e
brad fraizer <bradfraizer559@gmail.com>
2018-07-10 16:48:50 -0700

    commit before finishing takehome - trying to figure out why alerts aren't responsive to prompt response

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+let name = prompt("Greetings!  What is your name?");
+console.log(name);
+let color = prompt("Which would you say is your favorite color?", "Red, Pink, Blue, Purple, Yellow, Orange, Green, Brown, Black, and Gray.");
+  if (`${color} == blue`); {
+    alert(`${name}, did you know that blue is the color instils confidence and inspires feelings of trust, loyalty, integrity and responsibility?`);
+  } 
+  if (`${color} == red`); {
+    alert(`${name}, did you know that red is the color used universally to signify danger, courage, strength, and power?`);
+  }
+  if (`${color} == pink`); {
+    alert(`${name}, did you know that pink has associations with tenderness and nurturing while conveying a sense of safety and even vulnerability?`);
+  }
+  if (`${color} == purple`); {
+    alert(`${name}, did you know that purple is the color of the sensitive, compassionate intuitive soul – the introvert?`);
+  }
+  if (`${color} == yellow`); {
+    alert(`${name}, did you know that yellow stimulates the left side of the brain, helping with clear thinking and quick decision making?`);
+  }
+  if (`${color} == orange`); {
+    alert(`${name}, did you know that orange is the color of the extrovert, exuding happiness and joy, releasing inhibitions?`);
+  }
+  if (`${color} == green`); {
+    alert(`${name}, did you know that green evokes a feeling of abundance and a plentiful environment while providing a restful and secure feeling?`);
+  }
+  if (`${color} == brown`); {
+    alert(`${name}, did you know that, although frugal and stingy, brown is a color of safety and quiet confidence; warm and reassuring?`);
+  }
+  if (`${color} == black`); {
+    alert(`${name}, did you know that black is considered unfriendly and intimidating, yet still refined, elegant and confident?`);
+  }
+  if (`${color} == gray`); {
+    alert(`${name}, did you know that gray is linked with maturity and protection, being reliable and practical?`);
+  }
